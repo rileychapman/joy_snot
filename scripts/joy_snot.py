c96db0b10778b6664afed24fd53fb1be338b9c9e
vpreston <victorialynnpreston@gmail.com>
2015-03-06 01:36:14 -0500

    Fixing previous RC

--- scripts/joy_snot.py
+++ scripts/joy_snot.py
@@ -25,10 +25,8 @@ class MCN():
         self.z = 1000.0 #Throttle
         self.yaw = 1500 #Spin
         self.pub_rc = rospy.Publisher('/send_rc', roscopter.msg.RC)
-        self.pub_rc_2 = rospy.Publisher('/send_rc_2', roscopter.msg.RC)
         self.sub_joy = rospy.Subscriber("/joy", Joy, self.joy_callback)
         self.command_serv = rospy.ServiceProxy('command', APMCommand)
-        self.command_serv_2 = rospy.ServiceProxy('command_2', APMCommand)
 
         r = rospy.Rate(20)
         while not rospy.is_shutdown():
@@ -48,16 +46,13 @@ class MCN():
         if self.buttons:
             if self.buttons[2]:
                 self.command_serv(roscopter.srv.APMCommandRequest.CMD_ARM)
-                self.command_serv_2(roscopter.srv.APMCommandRequest.CMD_ARM)
                 print 'Arm Quad'
             if self.buttons[3]:
                 self.command_serv(4)
-                self.command_serv_2(4)
                 print 'Disarm Quad'
 
         (self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (int(self.x), int(self.y), int(self.z), int(self.yaw))
         self.pub_rc.publish(self.twist)
-        self.pub_rc_2.publish(self.twist)
 
 if __name__ == '__main__':
     print 'process started at ' + str(datetime.now())
